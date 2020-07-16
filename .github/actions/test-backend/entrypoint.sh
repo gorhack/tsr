bd6e5dca6594d9c48b7b216e8c94220676d42c65
Kyle Gorak <kyle@gorak.us>
2020-07-16 12:38:20 -0700

    test actions

---
+++ .github/actions/test-backend/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -eou pipefail
+
+if [[ -z "$GITHUB_WORKSPACE" ]]; then
+  echo "Set the GITHUB_WORKSPACE env variable."
+  exit 1
+fi
+
+if [[ -z "$GITHUB_REPOSITORY" ]]; then
+  echo "Set the GITHUB_REPOSITORY env variable."
+  exit 1
+fi
+
+cd $GITHUB_WORKSPACE
+# Run gradle tests
+# fail if tests fail
+{
+  ./gradlew test --info
+} ||
+  {
+    kill -9 "$(jobs -p)"
+    exit 1
+  }
+
+# Success if we get this far
+exit 0
---
+++ .github/actions/test-frontend/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -eou pipefail
+
+if [[ -z "$GITHUB_WORKSPACE" ]]; then
+  echo "Set the GITHUB_WORKSPACE env variable."
+  exit 1
+fi
+
+if [[ -z "$GITHUB_REPOSITORY" ]]; then
+  echo "Set the GITHUB_REPOSITORY env variable."
+  exit 1
+fi
+
+cd $GITHUB_WORKSPACE/client
+yarn install
+yarn lint:ci
+CI=true yarn test
