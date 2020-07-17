7b40762c37cae6a9dd0302def61427241de0e654
Kyle Gorak <kyle@gorak.us>
2020-07-17 14:02:56 -0700

    access jar artifact

--- pipeline/docker/app_container/build.sh
+++ pipeline/docker/app_container/build.sh
@@ -5,11 +5,10 @@ dockerImageName="docker.pkg.github.com/gorhack/tsr/app-container:latest"
 
 if [[ -n $GITHUB_REPOSITORY ]]; then
   dockerImageName="docker.pkg.github.com/$GITHUB_REPOSITORY:$GITHUB_REF"
-  cp build/libs/tsr-* ./pipeline/docker/app_container/tsr.jar
-  docker build -t $dockerImageName ./pipeline/docker/app_container/
-  rm ./pipeline/docker/app_container/tsr.jar
-else
-  cp ../../../build/libs/tsr-* ./tsr.jar
-  docker build -t $dockerImageName .
-  rm tsr.jar
 fi
+
+cp ../../../build/libs/*.jar ./tsr.jar
+
+docker build -t $dockerImageName .
+
+rm tsr.jar
