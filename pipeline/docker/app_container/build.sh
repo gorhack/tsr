13f0f2f71edfb8333e0ccb2fb1c22a94ecae4150
Kyle Gorak <kyle@gorak.us>
2020-07-17 15:01:04 -0700

    always push latest container

--- pipeline/docker/app_container/build.sh
+++ pipeline/docker/app_container/build.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
-# If script is run in pipeline, these will be set
-dockerImageName="docker.pkg.github.com/gorhack/tsr/app-container:latest"
-
-if [[ -n $GITHUB_REPOSITORY ]]; then
-  dockerImageName="docker.pkg.github.com/$GITHUB_REPOSITORY:$GITHUB_ACTION"
-fi
-
 cp ../../../build/libs/*.jar ./tsr.jar
 
-docker build -t $dockerImageName .
+docker build -t docker.pkg.github.com/gorhack/tsr/app-container:latest .
 
 rm tsr.jar
