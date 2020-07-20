0e4bb2bee46fd1db94b7fe99a1f29e2c4221f693
Kyle Gorak <kyle@gorak.us>
2020-07-20 07:53:43 -0700

    fail faster, fix aws...

--- .github/actions/deploy-eb/entrypoint.sh
+++ .github/actions/deploy-eb/entrypoint.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
-set -eou pipefail
-
-if [[ -z "$GITHUB_WORKSPACE" ]]; then
-  echo "Set the GITHUB_WORKSPACE env variable."
-  exit 1
-fi
-
-if [[ -z "$GITHUB_REPOSITORY" ]]; then
-  echo "Set the GITHUB_REPOSITORY env variable."
-  exit 1
-fi
-
-if [[ -z "$AWS_ACCESS_ID" ]]; then
-  echo "Set the AWS_ACCESS_ID env variable."
-  exit 1
-fi
-
-if [[ -z "$AWS_SECRET_KEY" ]]; then
-  echo "Set the AWS_SECRET_KEY env variable."
-  exit 1
-fi
+#set -eou pipefail
+#
+#if [[ -z "$GITHUB_WORKSPACE" ]]; then
+#  echo "Set the GITHUB_WORKSPACE env variable."
+#  exit 1
+#fi
+#
+#if [[ -z "$GITHUB_REPOSITORY" ]]; then
+#  echo "Set the GITHUB_REPOSITORY env variable."
+#  exit 1
+#fi
+#
+#if [[ -z "$AWS_ACCESS_ID" ]]; then
+#  echo "Set the AWS_ACCESS_ID env variable."
+#  exit 1
+#fi
+#
+#if [[ -z "$AWS_SECRET_KEY" ]]; then
+#  echo "Set the AWS_SECRET_KEY env variable."
+#  exit 1
+#fi
 
 echo "******* Installing EB CLI"
 # https://github.com/aws/aws-elastic-beanstalk-cli-setup
