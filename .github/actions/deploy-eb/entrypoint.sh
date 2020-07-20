0b0784ded2d4e29af5aa2bf86ec459078f272929
Kyle Gorak <kyle@gorak.us>
2020-07-20 08:08:46 -0700

    add git and pythong to path

--- .github/actions/deploy-eb/entrypoint.sh
+++ .github/actions/deploy-eb/entrypoint.sh
@@ -28,9 +28,10 @@ apt-get -y update \
     && apt-get -y install \
     git build-essential zlib1g-dev libssl-dev libncurses-dev \
     libffi-dev libsqlite3-dev libreadline-dev libbz2-dev curl
-build-essential zlib1g-dev libssl-dev libncurses-dev libffi-dev libsqlite3-dev libreadline-dev libbz2-dev
 git clone https://github.com/aws/aws-elastic-beanstalk-cli-setup.git
 ./aws-elastic-beanstalk-cli-setup/scripts/bundled_installer
+echo 'export PATH="/github/home/.ebcli-virtual-env/executables:$PATH"' >> ~/.bash_profile && source ~/.bash_profile
+echo 'export PATH=/github/home/.pyenv/versions/3.7.2/bin:$PATH' >> /github/home/.bash_profile && source /github/home/.bash_profile
 
 echo "******* Deploying to AWS EB"
 cd "$GITHUB_WORKSPACE"/pipeline/remote-docker || exit 1
