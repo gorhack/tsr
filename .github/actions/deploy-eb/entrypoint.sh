0cd65e27e614937860583c1a65f1c6f918338399
Kyle Gorak <kyle@gorak.us>
2020-07-20 08:16:39 -0700

    make bash

--- .github/actions/deploy-eb/entrypoint.sh
+++ .github/actions/deploy-eb/entrypoint.sh
@@ -1,26 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-#set -eou pipefail
-#
-#if [[ -z "$GITHUB_WORKSPACE" ]]; then
-#  echo "Set the GITHUB_WORKSPACE env variable."
-#  exit 1
-#fi
-#
-#if [[ -z "$GITHUB_REPOSITORY" ]]; then
-#  echo "Set the GITHUB_REPOSITORY env variable."
-#  exit 1
-#fi
-#
-#if [[ -z "$AWS_ACCESS_ID" ]]; then
-#  echo "Set the AWS_ACCESS_ID env variable."
-#  exit 1
-#fi
-#
-#if [[ -z "$AWS_SECRET_KEY" ]]; then
-#  echo "Set the AWS_SECRET_KEY env variable."
-#  exit 1
-#fi
+if [[ -z "$GITHUB_WORKSPACE" ]]; then
+  echo "Set the GITHUB_WORKSPACE env variable."
+  exit 1
+fi
+
+if [[ -z "$GITHUB_REPOSITORY" ]]; then
+  echo "Set the GITHUB_REPOSITORY env variable."
+  exit 1
+fi
+
+if [[ -z "$AWS_ACCESS_ID" ]]; then
+  echo "Set the AWS_ACCESS_ID env variable."
+  exit 1
+fi
+
+if [[ -z "$AWS_SECRET_KEY" ]]; then
+  echo "Set the AWS_SECRET_KEY env variable."
+  exit 1
+fi
 
 echo "******* Installing EB CLI"
 # https://github.com/aws/aws-elastic-beanstalk-cli-setup
@@ -30,7 +28,7 @@ apt-get -y update \
     libffi-dev libsqlite3-dev libreadline-dev libbz2-dev curl
 git clone https://github.com/aws/aws-elastic-beanstalk-cli-setup.git
 ./aws-elastic-beanstalk-cli-setup/scripts/bundled_installer
-echo 'export PATH="/github/home/.ebcli-virtual-env/executables:$PATH"' >> ~/.bash_profile && source ~/.bash_profile
+echo 'export PATH="/github/home/.ebcli-virtual-env/executables:$PATH"' >> /github/home/.bash_profile
 echo 'export PATH=/github/home/.pyenv/versions/3.7.2/bin:$PATH' >> /github/home/.bash_profile && source /github/home/.bash_profile
 
 echo "******* Deploying to AWS EB"
