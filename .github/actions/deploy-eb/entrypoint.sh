1567210377be50089e8113b11e0c9c21585fcef6
Kyle Gorak <kyle@gorak.us>
2020-07-20 08:30:42 -0700

    add docker config files

--- .github/actions/deploy-eb/entrypoint.sh
+++ .github/actions/deploy-eb/entrypoint.sh
@@ -24,9 +24,7 @@ fi
 
 echo "******* Installing EB CLI"
 # https://github.com/aws/aws-elastic-beanstalk-cli-setup
-apt-get -y update \
-    && apt-get -y install \
-    git build-essential zlib1g-dev libssl-dev libncurses-dev \
+apt-get -y install git zlib1g-dev libssl-dev libncurses-dev \
     libffi-dev libsqlite3-dev libreadline-dev libbz2-dev curl
 git clone https://github.com/aws/aws-elastic-beanstalk-cli-setup.git
 ./aws-elastic-beanstalk-cli-setup/scripts/bundled_installer
