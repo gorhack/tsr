700ce97bc442bad39bb431ba462455b294221b4b
Kyle Gorak <kyle@gorak.us>
2020-07-28 18:21:21 -0700

    upgrade gradlew

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,9 +29,8 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.flywaydb:flyway-core")
+	implementation("org.flywaydb:flyway-core:6.5.2")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
