269595b955576151b9622d45abc8063daf287965
Kyle Gorak <kyle@gorak.us>
2020-08-06 17:50:46 -0700

    Front end display user info

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 import "./App.css";
-import {User} from "./Users/User";
+import { User } from "./Users/User";
+import { Route, Switch } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 
 const App: React.FC = () => {
     return (
-        <div className="App">
-            <main className="App-content">
-                <User />
-                <div>Hello, world</div>
-                <div>v2</div>
-            </main>
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <main className="App-content">
+                    <Switch>
+                        <Route path="/">
+                            <User />
+                            <div>Hello, world</div>
+                            <div>v2</div>
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
+        </BrowserRouter>
     );
 };
 
