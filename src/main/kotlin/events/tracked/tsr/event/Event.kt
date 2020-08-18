7c3c9ee6f642dc86cdc838a40fc78fb127c21fe4
Riley McKenzie <ryemack@protonmail.com>
2020-08-18 12:15:14 -0700

    add event name to backend
    Signed-off-by: Kyle Gorak <kyle@gorak.us>

--- src/main/kotlin/events/tracked/tsr/event/Event.kt
+++ src/main/kotlin/events/tracked/tsr/event/Event.kt
@@ -10,6 +10,7 @@ data class Event(
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         var eventId: Long = 0,
+        var eventName: String? = "",
         var organization: String = "",
         var startDate: LocalDateTime = LocalDateTime.parse("1970-01-01T00:00:01"),
         var endDate: LocalDateTime = LocalDateTime.parse("1970-01-01T00:00:01"),
@@ -18,12 +19,13 @@ data class Event(
         @JoinColumn(name = "event_type_id", nullable = true)
         var eventType: EventType? = null
 ) : Auditable() {
-    constructor(eventId: Long, organization: String, startDate: LocalDateTime, endDate: LocalDateTime):
-            this(eventId=eventId, organization = organization, startDate = startDate, endDate = endDate, eventType = null)
+    constructor(eventId: Long, eventName: String?, organization: String, startDate: LocalDateTime, endDate: LocalDateTime):
+            this(eventId = eventId, eventName = eventName, organization = organization, startDate = startDate, endDate = endDate, eventType = null)
 
     fun toEventDTO(): EventDTO {
         return EventDTO(
                 eventId = eventId,
+                eventName = eventName,
                 organization = organization,
                 startDate = startDate,
                 endDate = endDate,
--- src/main/kotlin/events/tracked/tsr/event/EventDTO.kt
+++ src/main/kotlin/events/tracked/tsr/event/EventDTO.kt
@@ -4,16 +4,18 @@ import java.time.LocalDateTime
 
 data class EventDTO(
         val eventId: Long? = null,
+        val eventName: String? = "",
         val organization: String,
         val startDate: LocalDateTime,
         val endDate: LocalDateTime,
-        val eventType: EventType?,
+        val eventType: EventType? = null,
         val createdDate: LocalDateTime? = null,
         val lastModifiedDate: LocalDateTime? = null
 ) {
     constructor(event: Event) :
             this(
                     event.eventId,
+                    event.eventName,
                     event.organization,
                     event.startDate,
                     event.endDate,
