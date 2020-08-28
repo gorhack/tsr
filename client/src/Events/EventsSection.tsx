efefab03ec7a52991f5941b243b23d6d1941145a
Kyle Gorak <kyle@gorak.us>
2020-08-28 10:42:42 -0700

    show all events on main page
    Signed-off-by: Mark Zurat <mzurat@protonmail.com>

--- client/src/Events/EventsSection.tsx
+++ client/src/Events/EventsSection.tsx
@@ -1,12 +1,34 @@
-import React, { ReactElement } from "react";
-import { getAllEvents } from "./EventApi";
+import React, { ReactElement, useEffect, useState } from "react";
+import { getAllEvents, TsrEvent } from "./EventApi";
 
 export const EventsSection = (): ReactElement => {
-    const events = await getAllEvents();
+    const [eventList, setEventList] = useState<TsrEvent[]>([]);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const newEventList = await getAllEvents();
+                setEventList(newEventList);
+            } catch (e) {
+                console.error(`Error getting all events. ${e.message.error}`);
+            }
+        })();
+    }, [setEventList]);
+
+    const showEvents = (): ReactElement => {
+        return (
+            <>
+                {eventList.map((e) => (
+                    <span key={e.eventId}>{e.eventName}</span>
+                ))}
+            </>
+        );
+    };
+
     return (
         <>
             <h1>Heading for Events</h1>
-            <div>{events}</div>
+            {showEvents()}
         </>
     );
 };
--- client/src/__tests__/Events/EventsSection.test.tsx
+++ client/src/__tests__/Events/EventsSection.test.tsx
@@ -1,12 +1,34 @@
-import { render } from "@testing-library/react";
+import { act, render } from "@testing-library/react";
 import React from "react";
+import td from "testdouble";
 import { EventsSection } from "../../Events/EventsSection";
+import { TsrEvent } from "../../Events/EventApi";
+import { makeEvent } from "../TestHelpers";
+
+import * as EventApi from "../../Events/EventApi";
 
 describe("home page of the application", () => {
-    it("lists all created events", () => {
+    let mockGetAllEvents: typeof EventApi.getAllEvents;
+    beforeEach(() => {
+        mockGetAllEvents = td.replace(EventApi, "getAllEvents");
+    });
+
+    afterEach(td.reset);
+
+    it("lists all created events", async () => {
+        const eventList: TsrEvent[] = [
+            makeEvent({ eventId: 1, eventName: "barracks party" }),
+            makeEvent({ eventId: 2, eventName: "cq smash" }),
+        ];
+        const eventListPromise = Promise.resolve(eventList);
+        td.when(mockGetAllEvents()).thenDo(() => Promise.resolve(eventListPromise));
         const result = render(<EventsSection />);
+        await act(async () => {
+            await eventListPromise;
+        });
 
         console.log(result.container.innerHTML);
-        expect(result.getByText("event1")).toBeInTheDocument();
+        expect(result.getByText("barracks party")).toBeInTheDocument();
+        expect(result.getByText("cq smash")).toBeInTheDocument();
     });
 });
