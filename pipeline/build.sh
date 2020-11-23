f05ca63c0472e9d15fbbc42c942a7dceeae437a2
Kyle Gorak <kyle@gorak.us>
2020-11-23 13:13:05 -0800

    who am i

--- pipeline/build.sh
+++ pipeline/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-ls -al /usr/local/share/.cache/yarn
-rm -rf /usr/local/share/.cache/yarn
+#rm -rf /usr/local/share/.cache/yarn
 
 if [ -d "src/main/resources/static" ]; then
   rm -rf src/main/resources/static/*
