8bbb4338f142ead71a4a4c82681ee93ea1656b6b
Mark Zurat <mark.zurat@armyfc.onmicrosoft.com>
2021-04-11 18:40:43 -0700

    clears session cookies and local browser cache/storage on logout

--- src/main/kotlin/events/tracked/tsr/security/CommonSecurityConfigurer.kt
+++ src/main/kotlin/events/tracked/tsr/security/CommonSecurityConfigurer.kt
@@ -1,18 +1,30 @@
 package events.tracked.tsr.security
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.authentication.logout.HeaderWriterLogoutHandler
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository
+import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter
 import org.springframework.stereotype.Component
 
 @Component
 class CommonSecurityConfigurer : HttpSecurityConfigurer {
     override fun configure(http: HttpSecurity) {
         configureCsrfProtection(http)
+        clearBrowserDataOnLogout(http)
     }
 
     //line gives react the ability to read XSRF tokens
     // STIG ID: APSC-DV-002500
     private fun configureCsrfProtection(http: HttpSecurity) {
         http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+    }
+
+    //clears session cookies and the temporary local browser storage
+    //as per some STIG story
+    private fun clearBrowserDataOnLogout(http: HttpSecurity) {
+        val logoutHandler = HeaderWriterLogoutHandler(
+                ClearSiteDataHeaderWriter(ClearSiteDataHeaderWriter.Directive.ALL)
+        )
+        http.logout().addLogoutHandler(logoutHandler)
     }
 }
