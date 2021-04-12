5d90e62e6d924d14b6928956319ddab2ff560e67
Mark Zurat <mark.zurat@armyfc.onmicrosoft.com>
2021-04-11 18:26:32 -0700

    refactor how xsrf tokens are handled, in prep for more security configs

--- src/main/kotlin/events/tracked/tsr/security/SecurityConfiguration.kt
+++ src/main/kotlin/events/tracked/tsr/security/SecurityConfiguration.kt
@@ -1,21 +1,20 @@
-package events.tracked.tsr.config
+package events.tracked.tsr.security
 
-import events.tracked.tsr.config.CsrfHelpers.csrfHeaderFilter
-import events.tracked.tsr.config.CsrfHelpers.csrfTokenRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.web.session.SessionManagementFilter
 
+@Configuration
 @EnableWebSecurity
 class SecurityConfiguration : WebSecurityConfigurerAdapter() {
 
+    @Autowired
+    private lateinit var securityConfigurers: List<HttpSecurityConfigurer>
+
     @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
-        http.csrf().csrfTokenRepository(csrfTokenRepository())
-                .and().addFilterAfter(csrfHeaderFilter(true), SessionManagementFilter::class.java)
-                .headers().frameOptions().sameOrigin()
-
         http.authorizeRequests()
                 .antMatchers("/actuator/health", "/actuator/info").permitAll()
                 .anyRequest().authenticated()
@@ -27,5 +26,6 @@ class SecurityConfiguration : WebSecurityConfigurerAdapter() {
                 .userInfoEndpoint()
         http.oauth2ResourceServer().jwt()
 
+        securityConfigurers.forEach { it.configure(http) }
     }
 }
