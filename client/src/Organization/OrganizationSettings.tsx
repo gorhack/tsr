ec50878559ccc1a9da7d78beaf3a306d76f7045b
mojo9492 <mojo9492@outlook.com>
2021-06-13 12:48:16 -0700

    OrganizationSettings layout

--- client/src/Organization/OrganizationSettings.tsx
+++ client/src/Organization/OrganizationSettings.tsx
@@ -3,6 +3,9 @@ import UserContext from "../Users/UserContext";
 
 const OrganizationSettings: React.FC = () => {
     const tsrUser = useContext(UserContext);
+    const url = window.location.href;
+    const orgPath = url.length - 1;
+    const orgId = url[orgPath];
 
     useEffect(() => {
         if (tsrUser === undefined) {
@@ -19,11 +22,18 @@ const OrganizationSettings: React.FC = () => {
     }
 
     return (
-        <>
-            <h1 className="UserSettings-Header">{`${tsrUser.username} organization settings`}</h1>
-            <p>Role: {tsrUser.role}</p>
-            <p>{tsrUser.settings.organizations}</p>
-        </>
+        <div className="Event-Details-Container">
+            <h1 className="UserSettings-Header">{`${tsrUser.username} organization: ${orgId} settings`}</h1>
+            <div className="space-3" />
+            <form>
+                <h2>Role</h2>
+                <select defaultValue={undefined}>
+                    <option>---</option>
+                    <option value={`${tsrUser.userId}:${url}`}>Resourcer</option>
+                </select>
+                <div className="space-3" />
+            </form>
+        </div>
     );
 };
 
