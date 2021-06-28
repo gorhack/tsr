83b28ee433c9df0acfa5904805f937b1a2e4c278
Kyle Gorak <kyle@gorak.us>
2021-06-28 08:56:38 -0700

    Cleanup user promise awaiting in tests to eliminate testing errors within the UserContext

--- client/src/__tests__/Navigation/DrawerMenu.test.tsx
+++ client/src/__tests__/Navigation/DrawerMenu.test.tsx
@@ -5,7 +5,6 @@ import { DrawerMenu } from "../../Navigation/DrawerMenu";
 import { findByAriaLabel } from "../TestHelpers";
 import { createMemoryHistory, MemoryHistory } from "history";
 import { Router } from "react-router";
-import { UserContextProvider } from "../../Users/UserContext";
 
 describe("Drawer menu", () => {
     const CLOSE_MENU_BUTTON_NAME = "open menu";
@@ -80,11 +79,9 @@ describe("Drawer menu", () => {
         history.push("/somewhere");
 
         return render(
-            <UserContextProvider>
-                <Router history={history}>
-                    <DrawerMenu />
-                </Router>
-            </UserContextProvider>,
+            <Router history={history}>
+                <DrawerMenu />
+            </Router>,
         );
     };
 });
