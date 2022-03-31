gorhack/tsr

commit 845caab4bb325e4bfb891ed384eeffc48e42e947

Upgrade frontend dependencies and upgrade husky 6.0 prepare (#337)
* Upgrade frontend dependencies and upgrade husky 6.0 prepare

* Upgrade to gradle v7.1

* add the pre-commit

* upgrade front end deps

* upgrade backend deps

build.gradle.kts
@@ -4,7 +4,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.5.1"
+	id("org.springframework.boot") version "2.5.2"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	id("com.moowork.node") version "1.3.1"
 	id("org.flywaydb.flyway") version "7.10.0"
@@ -12,8 +12,8 @@ plugins {
 
 	id ("org.jetbrains.kotlin.plugin.jpa") version "1.5.0"
 	id ("org.jetbrains.kotlin.plugin.noarg") version "1.5.0"
-	kotlin("jvm") version "1.5.10"
-	kotlin("plugin.spring") version "1.5.10"
+	kotlin("jvm") version "1.5.20"
+	kotlin("plugin.spring") version "1.5.20"
 
 	id("com.github.ben-manes.versions") version "0.39.0" // helps find latest dependency versions `./gradlew dependencyUpdates`
 }
@@ -43,10 +43,10 @@ repositories {
 }
 
 var springSecurityVersion = "5.5.0"
-var springBootVersion = "2.5.1"
+var springBootVersion = "2.5.2"
 var keycloakVersion = "11.0.0"
 var jacksonVersion = "2.12.3"
-var jetBrainsKotlin = "1.5.10"
+var jetBrainsKotlin = "1.5.20"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa:${springBootVersion}")
@@ -72,7 +72,7 @@ dependencies {
 	implementation("org.springframework.security:spring-security-oauth2-jose:${springSecurityVersion}")
 	implementation("org.springframework.security:spring-security-oauth2-resource-server:${springSecurityVersion}")
 	implementation("org.springframework.security.oauth:spring-security-oauth2:2.5.1.RELEASE")
-	implementation("org.springframework.session:spring-session-jdbc:2.5.0")
+	implementation("org.springframework.session:spring-session-jdbc:2.5.1")
 
 	implementation("org.hibernate.validator:hibernate-validator-cdi:7.0.1.Final")
 

gradle/wrapper/gradle-wrapper.properties
@@ -1,5 +1,5 @@
 distributionBase=GRADLE_USER_HOME
 distributionPath=wrapper/dists
-distributionUrl=https\://services.gradle.org/distributions/gradle-6.8.3-bin.zip
+distributionUrl=https\://services.gradle.org/distributions/gradle-7.1-bin.zip
 zipStoreBase=GRADLE_USER_HOME
 zipStorePath=wrapper/dists

client/.husky/.gitignore
@@ -1,0 +1,1 @@
+_

client/.husky/pre-commit
@@ -1,0 +1,5 @@
+#!/bin/sh
+. "$(dirname "$0")/_/husky.sh"
+
+cd client
+yarn lint-staged

commit ee7214a82846c24bd651e6d3efa08723c8100976

Fixes yarnBuild gradle task with Gradle v7+ and upgrade to Gradle 7.4.2.
 - task broken in 845caab4bb325e4bfb891ed384eeffc48e42e947
 - moowork plugin no longer maintained resulting in broken gradle task
   - https://github.com/srs/gradle-node-plugin/issues/377

build.gradle.kts
@@ -1,4 +1,4 @@
-import com.moowork.gradle.node.yarn.YarnTask
+import com.github.gradle.node.yarn.task.YarnTask
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
@@ -6,7 +6,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.5.5"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	id("com.moowork.node") version "1.3.1"
+	id("com.github.node-gradle.node") version "3.2.1"
 	id("org.flywaydb.flyway") version "8.0.0"
 	id("org.sonarqube") version "3.3"
 
@@ -23,7 +23,10 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_15
 
 node {
-	nodeModulesDir = file("${project.projectDir}/client")
+	download.set(false)
+	yarnWorkDir.set(file("${project.projectDir}/client"))
+	nodeProjectDir.set(file("${project.projectDir}/client"))
+	yarnWorkDir.set(file("${project.projectDir}/.gradle/yarn"))
 }
 
 noArg {
@@ -108,14 +111,13 @@ tasks.withType<Test> {
 	}
 }
 
-tasks.register("yarnBuild", YarnTask::class) {
-	setWorkingDir("client")
-	args = listOf("build")
-	dependsOn("yarnInstall")
+tasks.getByName<YarnTask>("yarn_build") {
+	dependsOn("yarn_install")
 }
-tasks.register("yarnInstall", YarnTask::class) {
-	setWorkingDir("client")
-	args = listOf("install")
+
+tasks.getByName<YarnTask>("yarn_test") {
+	dependsOn("yarn_build")
+	environment.put("CI", "true")
 }
 
 tasks.getByName<Jar>("jar") {

gradle/wrapper/gradle-wrapper.properties
@@ -1,5 +1,5 @@
 distributionBase=GRADLE_USER_HOME
 distributionPath=wrapper/dists
-distributionUrl=https\://services.gradle.org/distributions/gradle-7.2-bin.zip
+distributionUrl=https\://services.gradle.org/distributions/gradle-7.4.2-bin.zip
 zipStoreBase=GRADLE_USER_HOME
 zipStorePath=wrapper/dists

run_jar.sh
@@ -84,7 +84,7 @@ echo ""
 
 if [[ -z ${CI+x} ]]; then
   # Not in CI
-  SPRING_PROFILES_ACTIVE="$env" ./gradlew yarnBuild assemble bootRun --console=plain --args="--server.port=$PORT" &
+  SPRING_PROFILES_ACTIVE="$env" ./gradlew yarn_build assemble bootRun --console=plain --args="--server.port=$PORT" &
 else
   # In CI
   SPRING_PROFILES_ACTIVE="$env" java -jar -Dserver.port=$PORT ./build/libs/tsr-*.jar &

test.sh
@@ -4,5 +4,7 @@ set -eou pipefail
 (cd client; yarn install;yarn lint:ci; CI=true yarn test;)
 ./gradlew clean build
 
-(cd client;CI=true yarn build)
+# Creates a jar for development/testing only. Not for use in production
+# https://stackoverflow.com/questions/16145522/chrome-showing-error-as-refused-to-execute-inline-script-because-of-content-sec
+(cd client;CI=true INLINE_RUNTIME_CHUNK=false yarn build)
 ./gradlew assemble