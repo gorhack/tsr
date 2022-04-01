9ef16e593565dffae011ff4b75d383bf842a4fbd
Kyle Gorak <kyle@gorak.us>
2022-04-01 08:53:02 -0400

    Updated backend dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,18 +4,18 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.5.5"
+	id("org.springframework.boot") version "2.6.6"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	id("com.github.node-gradle.node") version "3.2.1"
-	id("org.flywaydb.flyway") version "8.0.0"
+	id("org.flywaydb.flyway") version "8.5.5"
 	id("org.sonarqube") version "3.3"
 
-	id ("org.jetbrains.kotlin.plugin.jpa") version "1.5.0"
-	id ("org.jetbrains.kotlin.plugin.noarg") version "1.5.0"
-	kotlin("jvm") version "1.5.31"
-	kotlin("plugin.spring") version "1.5.31"
+	id ("org.jetbrains.kotlin.plugin.jpa") version "1.6.10"
+	id ("org.jetbrains.kotlin.plugin.noarg") version "1.6.10"
+	kotlin("jvm") version "1.6.10"
+	kotlin("plugin.spring") version "1.6.10"
 
-	id("com.github.ben-manes.versions") version "0.39.0" // helps find latest dependency versions `./gradlew dependencyUpdates`
+	id("com.github.ben-manes.versions") version "0.42.0" // helps find latest dependency versions `./gradlew dependencyUpdates`
 }
 
 group = "events.tracked.tsr"
@@ -45,18 +45,17 @@ repositories {
 	maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
-var springSecurityVersion = "5.5.2"
-var springBootVersion = "2.5.5"
-var keycloakVersion = "11.0.0"
-var jacksonVersion = "2.13.0"
-var jetBrainsKotlin = "1.5.31"
+var springSecurityVersion = "5.6.2"
+var springBootVersion = "2.6.5"
+var jacksonVersion = "2.13.2"
+var jetBrainsKotlin = "1.6.10"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa:${springBootVersion}")
 	implementation("org.springframework.boot:spring-boot-starter-web:${springBootVersion}")
 	implementation("org.springframework.boot:spring-boot-starter-actuator:${springBootVersion}")
 	implementation("org.springframework.boot:spring-boot-starter-websocket:${springBootVersion}")
-	implementation("org.flywaydb:flyway-core:8.0.0")
+	implementation("org.flywaydb:flyway-core:8.5.4")
 
 	// Deserialize json
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${jacksonVersion}")
@@ -75,14 +74,14 @@ dependencies {
 	implementation("org.springframework.security:spring-security-oauth2-jose:${springSecurityVersion}")
 	implementation("org.springframework.security:spring-security-oauth2-resource-server:${springSecurityVersion}")
 	implementation("org.springframework.security.oauth:spring-security-oauth2:2.5.1.RELEASE")
-	implementation("org.springframework.session:spring-session-jdbc:2.5.2")
+	implementation("org.springframework.session:spring-session-jdbc:2.6.2")
 
-	implementation("org.hibernate.validator:hibernate-validator-cdi:7.0.1.Final")
+	implementation("org.hibernate.validator:hibernate-validator-cdi:7.0.4.Final")
 
-	runtimeOnly("org.postgresql:postgresql:42.2.24")
+	runtimeOnly("org.postgresql:postgresql:42.3.3")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test:${springBootVersion}")
-	testImplementation("io.mockk:mockk:1.12.0")
+	testImplementation("io.mockk:mockk:1.12.3")
 }
 
 tasks.withType<KotlinCompile> {
